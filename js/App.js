54d250505673918df615c90990a11a600f5c2036
Alabi Yusuf <yusufolatunjialabi@gmail.com>
2022-04-24 13:28:17 +0100

    add

---
+++ js/App.js
@@ -0,0 +1,15 @@
+const Menu = document.querySelector(".menu")
+const Nav = document.querySelector(".righttop")
+const Menu2 = document.querySelector(".menu2")
+// const Close = document.querySelector(".menu")
+
+Menu.onclick = () => {
+    Nav.style.display = "block"
+
+}
+
+
+Menu2.onclick = () => {
+    Nav.style.display = "none"
+ 
+}
