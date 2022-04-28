dfa717e1535bb4884241f873579a5ff3b4181f26
Alabi Yusuf <yusufolatunjialabi@gmail.com>
2022-04-28 13:10:27 +0100

    2day commit

--- js/App.js
+++ js/App.js
@@ -2,6 +2,7 @@ const Menu = document.querySelector(".menu")
 const Nav = document.querySelector(".righttop")
 // const Close = document.querySelector(".menu")
 
+
 Menu.onclick = () => {
     // Nav.style.display = "block"
     Nav.classList.toggle("show")
